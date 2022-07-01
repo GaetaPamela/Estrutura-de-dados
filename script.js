c03eaee46a7283def54794659242a8096dcf0dbe
Gaeta Pamela <odontologia.pamela@gmail.com>
2022-07-01 14:03:59 -0300

    Inciando meu projeto

---
+++ script.js
@@ -0,0 +1,22 @@
+let estrutura = "Pamela"
+
+
+// Crie uma variável que receba um número;
+let variavel =  10
+
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let ano = 2022 - variavel
+console.log(ano)
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log( ano+variavel)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Mechendo no console.log");
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true 
+console.log(quartaFeira)
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira);
